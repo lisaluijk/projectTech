8bc3cb66cc0b377c93f716423a63e881148c328a
lisaluijk <lisa.luijkm@gmail.com>
2025-02-13 15:13:14 +0100

    create mapstructuur, create index.ejs

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views','view/pages');
+
+app.use('/static', express.static('static'))
+
+
 app.listen(8000);
+
 app.get('/', onHome);
 app.get('/egg', onEasterEgg);
 
 function onHome(req, res){
-    res.send('<h1>eerste server bitch</h1>');
+    res.render('index.ejs');
 }
 
 function onEasterEgg(req, res){
     res.send('<h1>ei</h1?');
 }
+
