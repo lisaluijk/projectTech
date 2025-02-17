3ecb8f52ab4877b6273beffed2a0e3e0cd8bf861
lisaluijk <lisa.luijkm@gmail.com>
2025-02-17 11:26:41 +0100

    install nodemon

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ app.use('/static', express.static('static'))
 app.listen(8000);
 
 app.get('/', onHome);
-app.get('/egg', onEasterEgg);
+app.get('/egg?', onEasterEgg);
+app.get('/submit', onSubmit);
 
 function onHome(req, res){
     res.render('index.ejs');
 }
 
 function onEasterEgg(req, res){
-    res.send('<h1>ei</h1?');
+    res.send('<h1>serveren</h1?');
+}
+
+function onSubmit(req,res){
+    res.render('submitForm.ejs');
 }
 
