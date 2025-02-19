aa00d965abfb1d964cd55ac23fefb578e2f4caa1
lisaluijk <lisa.luijkm@gmail.com>
2025-02-19 16:05:23 +0100

    add test page to understand partials and templates

--- server.js
+++ server.js
@@ -12,13 +12,24 @@ app.listen(8000);
 app.get('/', onHome);
 app.get('/egg?', onEasterEgg);
 app.get('/submit', onSubmit);
+app.get('/about', onAbout);
 
 app.use((req, res, next) => {
     res.render('error.ejs')
 })
 
 function onHome(req, res){
-    res.render('index.ejs');
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+      ];
+      var tagline = "No programming concept is complete without a cute animal mascot.";
+    
+    res.render('index.ejs', {
+        mascots: mascots,
+        tagline: tagline
+      });
 }
 
 function onEasterEgg(req, res){
@@ -27,5 +38,9 @@ function onEasterEgg(req, res){
 
 function onSubmit(req,res){
     res.render('submitForm.ejs');
+}
+
+function onAbout(req,res){
+    res.render('about.ejs');
 }
 
