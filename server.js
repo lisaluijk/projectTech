9119f4dd2988823e694eb871c49dcfe620123931
lisaluijk <lisa.luijkm@gmail.com>
2025-02-27 13:16:52 +0100

    add mongodb and a whole lot of other shit

--- server.js
+++ server.js
@@ -1,22 +1,44 @@
+require('dotenv').config();
+
+const { MongoClient, ObjectId } = require("mongodb");
+const uri = process.env.URI;
+const client = new MongoClient(uri);
+const db = client.db(process.env.DB_NAME);
+
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views','view/pages');
 
-app.use('/static', express.static('static'))
+app.use('/static', express.static('static'));
+app.use(express.urlencoded({extended: true}));
 
 
 app.listen(8000);
 
 app.get('/', onHome);
 app.get('/egg?', onEasterEgg);
-app.get('/submit', onSubmit);
 app.get('/about', onAbout);
+app.get("/form", onForm);
+app.post('/submit', onSubmit);
+//app.get('/submit', onSubmit);
 
 app.use((req, res, next) => {
-    res.render('error.ejs')
+    res.render('error.ejs');
 })
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("client connected to database");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB();
+
 
 function onHome(req, res){
     var mascots = [
@@ -37,10 +59,19 @@ function onEasterEgg(req, res){
 }
 
 function onSubmit(req,res){
-    res.render('submitForm.ejs');
+    //res.render('submitForm.ejs');
+    //const { username, password, ofcourse, zekers, favcolor } = req.body;
+    res.send(`${req.body.username}`);
+    console.log(req.body);
 }
 
 function onAbout(req,res){
     res.render('about.ejs');
 }
+
+function onForm(req, res){
+    console.log("lisa is poepie, soortvan lief en stinky");
+    res.render('form.ejs');
+}
+
 
